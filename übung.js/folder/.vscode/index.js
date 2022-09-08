5a0383d87d24e9de0c3041f097050378f256fe66
AN.SZ <sza28088@gmail.com>
2022-09-08 10:31:56 +0200

    culcaltor

---
+++ übung.js/folder/.vscode/index.js
@@ -0,0 +1,56 @@
+//array object
+const cars = ["saab","Volvo", "BMW"];//
+console.log(cars);
+
+//array list
+let car1 = "Saab";// 
+let car = "Volvo";//
+let car3 = "BMW";//
+console.log(car1 ,2 ,3);//
+
+// array spaces and line
+const cars = [//
+    "Saab",//
+    "Volvo",//
+    "BMW",//
+];//
+
+// create array and provide elements
+const gars = [];//
+cars[0] = "Saab";
+cars[1] = "Volvo";
+cars[2] = "BMW";
+
+//js new keyword [new array]
+const cars = new Array("Saab", "Volvo", "BMW");
+
+//accessing array element
+const cars = ["Saab", "Volvo", "BMW"];
+
+
+//changing array element
+const cars = ["saab", "volvo", "BMW"];
+cars [0] = "Ople";
+
+// array use numbers
+const person = ["john", "Doe", 45];
+console.log(person);
+
+//  array elements can be  object
+myArray[0] = Date. now;
+myArra [1] = myFunktion;
+myArray [2] = myCars;
+
+//kill rob (=slice)
+const arrLeftBehind = arrpeople.slice(3, 1)
+console.log({arrLeftBehind})
+console.log({arrpeople})
+
+//pop something => changes the original 
+// arrpeople.pop()
+const whatever = arrpeople.slice(-2)
+console.log({whatever})
+
+//replace
+const something = "Hello" // => "oellh"
+console.log(something[something.length-1] + something)
